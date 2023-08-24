a624d59c8bbeda02f1db337530c61ddfb884ef47
Zikra Parween <90086666+ZikraShakil@users.noreply.github.com>
2023-08-24 15:27:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const byteSize = (str) => {
-  // write your code here
+  // Create a Blob object with the string data
+  const blob = new Blob([str]);
+
+  // Get the size of the Blob in bytes
+  const sizeInBytes = blob.size;
+
+  return sizeInBytes;
 };
 
 // Do not change the code below
